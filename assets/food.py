90bc0ba64f9dc671dbf1c504b4d9a95358f3cf2e
FinJG <finlay.griffiths03@gmail.com>
2024-05-19 21:46:32 +0100

    food now varies in colour

--- assets/food.py
+++ assets/food.py
@@ -8,14 +8,14 @@ class Food:
 
         self.pos = pygame.Vector2(x, y)
         self.chunks = 10
-        self.col = (randint(50, 100), randint(200, 255), randint(50, 100))
+        self.col = (randint(50, 100), randint(150, 200), randint(50, 100))
 
     def update(self):
         if self.chunks == 0:
             self.game.foods.remove(self)
 
     def draw(self):
-        pygame.draw.rect(self.game.screen, (200, 0, 0),
+        pygame.draw.rect(self.game.screen, self.col,
                          (self.pos.x * self.game.tile_size, self.pos.y * self.game.tile_size,
                           self.game.tile_size, self.game.tile_size))
 
