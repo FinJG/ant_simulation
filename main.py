33ff6ef37ecc161eaeb1272001c3a3e36d62e817
FinJG <finlay.griffiths03@gmail.com>
2024-05-20 19:50:21 +0100

    added debug tools

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Game(assets.Settings):
                                              randint(0, self.grid_size),
                                              randint(0, self.grid_size),
                                              1,
-                                             (randint(0, 255), randint(0, 255), randint(0, 255))
+                                             (randint(50, 200), randint(50, 200), randint(50, 200))
                                              ))
 
         for i in range(200):
@@ -31,6 +31,8 @@ class Game(assets.Settings):
 
     def events(self):
         self.keys = pygame.key.get_pressed()
+        self.mouse_pos = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -47,11 +49,35 @@ class Game(assets.Settings):
                 if event.key == pygame.K_r:
                     self.new_game()
 
+                if event.key == pygame.K_LEFTBRACKET:
+                    if self.displaying_path_index - 1 >= 0:
+                        self.displaying_path_index -= 1
+
+                if event.key == pygame.K_RIGHTBRACKET:
+                    if self.displaying_path_index + 1 < len(self.paths_to_food):
+                        self.displaying_path_index += 1
+
         if self.keys[pygame.K_w]:
             self.FPS += 0.1 * self.dt
 
         if self.keys[pygame.K_s]:
             self.FPS -= 0.1 * self.dt
+
+        # pygame.display.set_caption(f' ants: {[[ant.health for ant in nest.ants] for nest in self.nests]}')
+
+        for food in self.foods:
+            if food.pos == (self.mouse_pos[0] // self.tile_size, self.mouse_pos[1] // self.tile_size):
+                pygame.display.set_caption(f'{food.chunks} ants: {[[ant.health for ant in nest.ants] for nest in self.nests]}')
+            # else:
+        self.paths_to_food = []
+        for nest in self.nests:
+            for ant in nest.ants:
+                if ant.pheromones_to_food:
+                    self.paths_to_food.append(ant.pheromones_to_food)
+
+        if self.displaying_path_index > len(self.paths_to_food) - 1:
+            self.displaying_path_index = len(self.paths_to_food) - 1
+
 
     def update(self):
         for nest in self.nests:
@@ -71,7 +97,11 @@ class Game(assets.Settings):
             food.update()
 
         self.dt = self.clock.tick(self.FPS)
-        pygame.display.set_caption(f'{self.clock.get_fps():.1f}')
+
+        # debug tools
+        # pygame.display.set_caption(f'{self.clock.get_fps():.1f}')
+        # pygame.display.set_caption(f'ants: {sum([len(nest.ants) for nest in self.nests])}, total_food: {[nest.stored_food for nest in self.nests]}')
+        # pygame.display.set_caption(f'total_food: {[nest.stored_food for nest in self.nests]} ants: {[ant.health for ant in nest.ants for nest in self.nests]}')
 
     def draw(self):
         self.screen.fill((235, 235, 235))
@@ -83,6 +113,10 @@ class Game(assets.Settings):
 
         for food in self.foods:
             food.draw()
+
+        if self.paths_to_food:
+            if len(self.paths_to_food[self.displaying_path_index]) > 2:
+                pygame.draw.lines(self.screen, (255, 0, 0), False, self.paths_to_food[self.displaying_path_index], 2)
 
         pygame.display.update()
 
